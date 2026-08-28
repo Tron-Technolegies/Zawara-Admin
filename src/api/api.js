00a345d62265625476e39fb4cf4524fefd719c2a
savithnarayan111 <savithnarayan111@gmail.com>
2026-08-28 14:55:17 +0530

    changed url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "https://zawara-backend-23bb.onrender.com/api/admin/",
+    baseURL: "https://zawara-backend-23bb.onrender.com",
     // baseURL: "http://127.0.0.1:8000/api/admin/",
 });
 
